2a9af62eadfbb76f7c7b8c8d1d430d37d44acbd4
Sarthak Vaishampayan <149432829+SarthakVaishampayan@users.noreply.github.com>
2024-09-27 03:26:03 +0530

    font style added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ export default function Navbar() {
   let navbar_style = {
    
     display:"flex",
+    justifyContent: "space-evenly",
     alignItems: "center",
     position: "fixed",
     top: "5%",
@@ -13,12 +14,14 @@ export default function Navbar() {
     height: "7vh",
     width: "80%",
     borderRadius: "30px",
+    fontSize:"140%",
     boxShadow:
       "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
   };
-  return <div className="font" style={navbar_style}>
-    <div  >Sarthak Vaishampayan</div>
+  return <div style={navbar_style}>
+    <div className="font" >Sarthak Vaishampayan</div>
 
   </div>;
+
 
 }
