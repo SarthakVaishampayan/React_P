3c115be32e49a068fa5eb4f90131a8d38cac9ddf
Sarthak Vaishampayan <149432829+SarthakVaishampayan@users.noreply.github.com>
2024-09-28 10:22:18 +0530

    home page designed

---
+++ src/components/Home.js
@@ -0,0 +1,72 @@
+import React from "react";
+import pp3 from "../assests/pp3.png";
+
+export default function home_content() {
+  let home_content_style = {
+    display: "flex",
+    alignItems: "center",
+    //  border: "1px solid black",
+    position: "relative",
+    top: "20vh",
+    height: "70vh",
+    justifyContent: "space-around",
+  };
+  let img_home = {
+    height: "60vh",
+    position: "relative",
+    right: "25vh",
+  };
+
+  return (
+    <>
+      <div style={home_content_style}>
+        <div
+          style={{
+            height: "100%",
+            width: "55%",
+            display: "grid",
+            gridTemplateRows: "25% 7% 10% 10% 10% 5% 10% 20%",
+            gridTemplateColumns: "25% auto",
+          }}
+        >
+          <div
+            className="font"
+            style={{
+              fontSize: "x-large",
+              gridRowStart: "2",
+              gridRowEnd: "3",
+              gridColumnStart: "2",
+            }}
+          >
+            Hi! I'm Sarthak
+          </div>
+          <div
+            className="font"
+            style={{
+              gridRowStart: "3",
+              gridRowEnd: "6",
+              gridColumnStart: "2",
+              fontSize: "7vh",
+            }}
+          >
+            I am a Web Developer and I enjoy designing websites
+          </div>
+          <div
+            className="font"
+            style={{
+              gridRowStart: "7",
+              gridRowEnd: "8",
+              gridColumnStart: "2",
+              // fontSize: "7vh",
+            }}
+          >
+            <button className="font" style={{ fontSize: "small", height: "60%", width: "20%", backgroundColor: "#fff019", borderWidth: "0px", borderRadius: "5px" }}>Get in Touch</button>
+          </div>
+        </div>
+        <div>
+          <img style={img_home} src={pp3} alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
+import sv from "../assests/sv.png";
 
 export default function Navbar() {
   let navbar_style = {
-   
-    display:"flex",
+    display: "flex",
     justifyContent: "space-evenly",
     alignItems: "center",
     position: "fixed",
@@ -13,15 +13,45 @@ export default function Navbar() {
     opacity: "60%",
     height: "7vh",
     width: "80%",
-    borderRadius: "30px",
-    fontSize:"140%",
+    borderRadius: "25px",
+    fontSize: "140%",
     boxShadow:
       "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
   };
-  return <div style={navbar_style}>
-    <div className="font" >Sarthak Vaishampayan</div>
 
-  </div>;
-
-
+  let menu = {
+    justifyContent: "space-evenly",
+    width: "40%",
+  };
+  let logo = {
+    height: "35px",
+    position: "relative",
+    top: "2.5px",
+  };
+  return (
+    <>
+      <div className="App" style={navbar_style}>
+        <div style={{
+          position: "relative",
+          left: "-90px"
+}} className="font flex">
+          <div>
+            <img  style={logo} src={sv} alt="SV" />
+          </div>
+          <div style={{
+            display: "flex",
+            alignItems: "center",
+            marginLeft:"20px"}}>
+            Sarthak Vaishampayan
+          </div>
+        </div>
+        <div style={menu} className="flex font">
+          <div>Home</div>
+          <div>Resume</div>
+          <div>Expereince</div>
+          <div>About</div>
+        </div>
+      </div>
+    </>
+  );
 }
